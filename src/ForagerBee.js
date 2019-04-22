80988fb845e00e6193549cf9952baa0a8ea1a4b0
jasminelopez <lopez.jasmine094@gmail.com>
2019-04-22 12:52:20 -0700

    complete es6 classes for all bees

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,12 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends Bee {
+  constructor (age) {
+    super(age, age);
+    this.age = 10;
+    this.job ='find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+  forage () {
+    this.treasureChest.push('treasure');
+  }
 };
